f4dec7a6c6939b58d2a58f545f3d132ab214ff7c
JJ Lopez <lowlifebob@gmail.com>
2022-04-27 10:27:41 -0400

    Initial commit

---
+++ utils.py
@@ -0,0 +1,53 @@
+products = {
+    "video_cards": [
+        {"model": "1030", "riid": "I1PJ7P2DX4GJ4I","asin": "B07Q3YFLVD"},
+        
+        {"model": "3080", "riid": "B08HHDP9DW", "asin": "I1O2EXHNBVM3Y7"},
+        {"model": "3080", "riid": "B08R14HX34", "asin": "IWOH9RAG6EGG6"},
+        {"model": "3080", "riid": "B08J6F174Z", "asin": "I3GHOH1AVDQWBN"},
+        {"model": "3080", "riid": "B08HVV2P4Z", "asin": "I1B1Y3SGAAGLZL"},
+        {"model": "3080", "riid": "B08HBR7QBM", "asin": "I1NLOTOJGASCVQ"},
+        {"model": "3080", "riid": "B08HR5SXPS", "asin": "I2EE8A6WS9QRI0"},
+        {"model": "3080", "riid": "B08QW8BKDV", "asin": "I3OVPM9GRBQIX0"},
+        {"model": "3080", "riid": "B08HR7SV3M", "asin": "IRU46JUBS4087"},
+        {"model": "3080", "riid": "B08KGZVKXM", "asin": "I14HEI6SN01H7T"},
+        {"model": "3080", "riid": "B08HJTH61J", "asin": "I38SE4M52YS0CA"},
+        {"model": "3080", "riid": "B08HJS2JLJ", "asin": "I2OQ3E4FVG19IQ"},
+        {"model": "3080", "riid": "B08KJ3VKLQ", "asin": "IOVLKJMSMU5G1"},
+        {"model": "3080", "riid": "B08HR55YB5", "asin": "I1Y4FVMGBX0NFZ"},
+        {"model": "3080", "riid": "B08HR4RJ3Q", "asin": "I3O0NEM1R7TZJM"},
+        {"model": "3080", "riid": "B08HR6FMF3", "asin": "I1NTN3VLN3BXL3"},
+
+        {"model": "3070", "riid": "B08KY266MG", "asin": "I2KBUUFQTMO32C"},
+        {"model": "3070", "riid": "B08HBF5L3K", "asin": "I1NEIJKGUU52PP"},
+        {"model": "3070", "riid": "B08KY322TH", "asin": "I309YVY7X8UBCQ"},
+        {"model": "3070", "riid": "B08KXZV626", "asin": "I3SW1GWO1C8K9R"},
+        {"model": "3070", "riid": "B08L8L71SM", "asin": "IQO0996SZB9YJ"},
+        {"model": "3070", "riid": "B08L8KC1J7", "asin": "I3L0E464XXS0K"},
+        {"model": "3070", "riid": "B08M14Y3C7", "asin": "IHSTOON5215JQ"},
+        {"model": "3070", "riid": "B08MT6B58K", "asin": "I31OZARI8WSFB2"},
+        {"model": "3070", "riid": "B08L8LG4M3", "asin": "I3BA4P8UI1XP1W"},
+        {"model": "3070", "riid": "B08L8HPKR6", "asin": "I3Q2D25JOCVYTI"},
+        {"model": "3070", "riid": "B08HBJB7YD", "asin": "I1EOH2R6MPEOAD"},
+
+        {"model": "3060ti", "riid": "B08NYPKW1Z", "asin": "I3CCUJ3PE2TSEB"},
+        {"model": "3060ti", "riid": "B08P3V572B", "asin": "I1LXJ37ZD1KB1E"},
+        {"model": "3060ti", "riid": "B08NW5HNYW", "asin": "II55373DJW2P3"},
+        {"model": "3060ti", "riid": "B08NW693LG", "asin": "I3J42DASWH61H9"},
+        {"model": "3060ti", "riid": "B08NYPLXPJ", "asin": "ITXL40RTH3SUF"},
+        {"model": "3060ti", "riid": "B08NYP7KG6", "asin": "I222VP2WBU1SKF"},
+        {"model": "3060ti", "riid": "B08NYNJ6RC", "asin": "I1AEUV550JCXOV"},
+        {"model": "3060ti", "riid": "B08Q8QR7PK", "asin": "I3EHYGJ5SQFX3M"},
+        {"model": "3060ti", "riid": "B083Z7TR8Z", "asin": "I2GXCX8W3QINYE"},
+        {"model": "3060ti", "riid": "B08P2H5LW2", "asin": "I9Y6HN4CJD6RW"},
+        {"model": "3060ti", "riid": "B08R876RTH", "asin": "I2JAIPWU3QKG29"},
+        {"model": "3060ti", "riid": "B083Z5P6TX", "asin": "I33FDGN9X4DNLY"},
+        {"model": "3060ti", "riid": "B08P2D1JZZ", "asin": "I3UJ2335WKMY0O"},        
+    ]
+}
+
+riid = "B08NW5HNYW"
+
+for item in products["video_cards"]:
+    if item["riid"] == "B08NW5HNYW":
+        print(item)
